73f8637a1c44e85b764d3c4411f98f400bfc240d
Sqw3EgL <sqw3egl@protonmail.com>
2025-12-29 15:38:47 +0000

    tenant id var

--- infra/variables.tf
+++ infra/variables.tf
@@ -16,6 +16,12 @@ variable "environment" {
   default     = "dev"
 }
 
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+  default = "21cf3499-0582-4344-9a87-78253166a61a"
+}
+
 variable "tags" {
   description = "Tags for resources"
   type        = map(string)
